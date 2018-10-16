3e5e46108b554857418971dadafeede357574598
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2018-10-16 12:01:30 +0200

    Changing the plugin description

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Chimera ready to use in scipion.',  # Required
+    description='Plugin to use chimera programs within the Scipion framework',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
