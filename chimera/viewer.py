9fe19858c292c89b0bba1cbd32c5869583452705
marta <mmmtnez@cnb.csic.es>
2018-12-12 12:27:12 +0100

    Modification of imports according Chimera plug-in

--- chimera/viewer.py
+++ chimera/viewer.py
@@ -31,9 +31,8 @@ from pyworkflow.em.convert import ImageHandler
 from chimera.protocols.protocol_fit import ChimeraProtRigidFit
 from chimera.protocols.protocol_operate import ChimeraProtOperate
 from chimera.protocols.protocol_restore import ChimeraProtRestore
-from pyworkflow.em.viewers.viewer_chimera import (createCoordinateAxisFile,
-                                                  runProgram, getProgram,
-                                                  sessionFile)
+from pyworkflow.em.viewers.viewer_chimera import Chimera, sessionFile
+
 from pyworkflow.viewer import DESKTOP_TKINTER, Viewer
 
 
@@ -76,7 +75,7 @@ class ChimeraViewerBase(Viewer):
 
         bildFileName = os.path.abspath(self.protocol._getTmpPath(
             "axis_output.bild"))
-        createCoordinateAxisFile(dim,
+        Chimera.createCoordinateAxisFile(dim,
                                  bildFileName=bildFileName,
                                  sampling=sampling)
         fnCmd = self.protocol._getTmpPath("chimera_output.cmd")
@@ -105,7 +104,7 @@ class ChimeraViewerBase(Viewer):
         f.close()
 
         # run in the background
-        runChimeraProgram(getProgram(), fnCmd+"&")
+        Chimera.runProgram(fnCmd+"&")
         return []
 
 class ChimeraRestoreViewer(Viewer):
@@ -124,7 +123,7 @@ class ChimeraRestoreViewer(Viewer):
                 self.protocol.inputProtocol.get()._getExtraPath(), sessionFile)
             path = os.path.abspath(path2)
 
-        runChimeraProgram(getProgram(), path  + "&")
+            Chimera.runProgram(path  + "&")
         return []
 
 
