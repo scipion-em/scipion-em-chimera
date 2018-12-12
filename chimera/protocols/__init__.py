53a651be002dcacdcc412dd766e35abb08727547
marta <mmmtnez@cnb.csic.es>
2018-12-12 12:01:14 +0100

    added ChimeraModelFromTemplate

--- chimera/protocols/__init__.py
+++ chimera/protocols/__init__.py
@@ -2,3 +2,4 @@ from protocol_fit import ChimeraProtRigidFit
 from protocol_operate import ChimeraProtOperate
 from protocol_restore import ChimeraProtRestore
 from protocol_base import ChimeraProtBase
+from protocol_modeller_search import ChimeraModelFromTemplate
