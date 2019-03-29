29c17717f4224d2554b8669f4c3fad420ce69239
marta <mmmtnez@cnb.csic.es>
2019-03-29 16:49:31 +0100

    Addition of two tests for symmetries C1 and C2

--- chimera/tests/test_protocol_contact.py
+++ chimera/tests/test_protocol_contact.py
@@ -23,40 +23,111 @@
 # ***************************************************************************/
 
 
-from os.path import exists
+import os
 from collections import Counter
 from pyworkflow.em.constants import (SYM_I222, SYM_I222r, SYM_In25, SYM_In25r,
                                      SYM_CYCLIC, SYM_DIHEDRAL, SYM_TETRAHEDRAL,
                                      SYM_OCTAHEDRAL, SCIPION_SYM_NAME)
 
 from chimera.protocols import ChimeraProtContacts
-from pyworkflow.tests import BaseTest, setupTestProject
+from pyworkflow.tests import BaseTest, setupTestProject, DataSet
 from pyworkflow.em.protocol.protocol_import import ProtImportPdb
+
 
 class TestImportBase(BaseTest):
     @classmethod
     def setUpClass(cls):
         setupTestProject(cls)
+        cls.dsModBuild = DataSet.getDataSet('model_building_tutorial')
 
-class TestChimeraContact(TestImportBase):
-    # protocol to test the chimera computed contacts between pairs
-    # of chains
-
-    def _importStructurePDB(self, pdbID='6b1t'):
+class TestImportData(TestImportBase):
+    """ Import atomic structures(PDBx/mmCIF files)
+    """
+    def _importStructureFromPDBId(self, pdbID):
         args = {'inputPdbData': ProtImportPdb.IMPORT_FROM_ID,
                 'pdbId': pdbID
                 }
         protImportPDB = self.newProtocol(ProtImportPdb, **args)
-        protImportPDB.setObjLabel('import pdb\n %s' % pdbID)
+        protImportPDB.setObjLabel('import structure\n%s' % pdbID)
         self.launchProtocol(protImportPDB)
+        self.assertTrue(protImportPDB.outputPdb.getFileName())
         return protImportPDB.outputPdb
 
 
-    def testContacts(self):
-        #import sequence
-        pdb1 = self._importStructurePDB('6b1t') # A
+    def _importStructureFromFile(self, fileName):
+        args = {'inputPdbData': ProtImportPdb.IMPORT_FROM_FILES,
+                'pdbFile': self.dsModBuild.getFile(fileName)
+                }
+        protImportPDB = self.newProtocol(ProtImportPdb, **args)
+        protImportPDB.setObjLabel('import structure\n%s' %
+                                  os.path.basename(fileName))
+        self.launchProtocol(protImportPDB)
+        self.assertTrue(protImportPDB.outputPdb.getFileName())
+        return protImportPDB.outputPdb
+
+
+class TestChimeraContact(TestImportData):
+    # protocol to test the chimera computed contacts between pairs
+    # of chains
+
+    def testContactsAsymetry(self):
+        # import PDB; whole hemoglobin macromolecule with HEM groups as
+        # independent chains
+        pdb1 = self._importStructureFromFile('PDBx_mmCIF/5ni1_HEM.cif')
+        args = {'pdbFileToBeRefined': pdb1,
+                'chainStructure': '{"A": "chainA", "A002": "HEM_A", '
+                                  '"B": "chainB", "B002": "HEM_B", '
+                                  '"C": "chainC", "C002": "HEM_C", '
+                                  '"D": "chainD", "D002": "HEM_D"}',
+                'symmetryGroup': SYM_CYCLIC,
+                'symmetryOrder': 1
+                }
+
+        protContacts = self.newProtocol(ChimeraProtContacts, **args)
+        protContacts.setObjLabel('5ni1_HEM\ncontacts')
+        self.launchProtocol(protContacts)
+
+        c, conn = protContacts.prepareDataBase(drop=False)
+        tableName = protContacts.getTableName()
+        sqlCommand = """SELECT count(*) FROM {tableName}""".format(tableName=tableName)
+        c.execute(sqlCommand)
+        row = c.fetchone()
+        self.assertEqual(int(row[0]), 736)
+
+    def testContactsSymC2(self):
+        # import PDB; unit cell of hemoglobin macromolecule with HEM groups as
+        # independent chains; sym C2
+        pdb1 = self._importStructureFromFile('PDBx_mmCIF/5ni1_unit_cell_HEM.cif')
+        args = {'pdbFileToBeRefined': pdb1,
+                'chainStructure': '{"A": "chainA", "A002": "HEM_A", '
+                                  '"B": "chainB", "B002": "HEM_B"}',
+                'symmetryGroup': SYM_CYCLIC,
+                'symmetryOrder': 2
+                }
+
+        protContacts = self.newProtocol(ChimeraProtContacts, **args)
+        protContacts.setObjLabel('5ni1_unit_cell_HEM\ncontacts')
+        self.launchProtocol(protContacts)
+
+        c, conn = protContacts.prepareDataBase(drop=False)
+        tableName = protContacts.getTableName()
+        sqlCommand = """SELECT count(*) FROM {tableName}""".format(tableName=tableName)
+        c.execute(sqlCommand)
+        row = c.fetchone()
+        self.assertEqual(int(row[0]), 380)
+
+    def testContactsSymI222(self):
+        """
+        This test assesses contacts between any couple of proteins of the
+        unit cell of an icosahedral virus and between any protein of the unit
+        cell and any protein of the neighbour unit cells.
+        """
+        # import structure of the unit cell of a icosahedral virus
+        pdb1 = self._importStructureFromPDBId('6b1t') # A
         args = {'pdbFileToBeRefined': pdb1,
                 'symmetryGroup': SYM_I222,
+                # symmetry group of the whole virus, once you have applied
+                # symmetry to the unit cell
                 'chainStructure': '{"A": "h1", "B": "h1", "C": "h1", '
                                   '"D": "h2", "E": "h2", "F": "h2", '
                                   '"G": "h3", "H": "h3", "I": "h3", '
@@ -67,10 +138,11 @@ class TestChimeraContact(TestImportBase):
                                   '"T": "ixb", "U": "vi", '
                                   '"V": "vi", "W": "vii", '
                                   '"X": "x", "Y": "vi"}'
+                # labeling of unit cell chains
                 }
 
         protContacts = self.newProtocol(ChimeraProtContacts, **args)
-        protContacts.setObjLabel('chain pairs')
+        protContacts.setObjLabel('6b1t\nicosahedral virus\nsym I222\ncontacts ')
         self.launchProtocol(protContacts)
         # TODO: more test are needed may be with an smaller sample
         c, conn = protContacts.prepareDataBase(drop=False)
@@ -78,4 +150,5 @@ class TestChimeraContact(TestImportBase):
         sqlCommand = """SELECT count(*) FROM {tableName}""".format(tableName=tableName)
         c.execute(sqlCommand)
         row = c.fetchone()
-        self.assertEqual(int(row[0]), 488698)
+        # self.assertEqual(int(row[0]), 488698)
+        self.assertEqual(int(row[0]), 363616)
