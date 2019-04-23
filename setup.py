fd269c932304609818b588ce5f4281faf372874f
marta <mmmtnez@cnb.csic.es>
2019-04-23 12:30:42 +0200

    Updated plugin version to 1.0.0b12

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='scipion-em-chimera',  # Required
-    version='1.0.0b11',  # Required
+    version='1.0.0b12',  # Required
     description='Plugin to use chimera programs within the Scipion framework',  # Required
     long_description=long_description,  # Optional
     url='https://github.com/scipion-em/scipion-em-chimera',  # Optional
