4c3ba79de3cc6ec9dd7d314f93115d0d54f81d8c
rmarabini <roberto@cnb.csic.es>
2022-04-11 18:01:08 +0200

    zip file to be downloaded

--- chimera/colabs/test_colab.py
+++ chimera/colabs/test_colab.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
 df.to_csv('dataframe.csv')
-
-files.download('dataframe.csv')
+!zip -q results.zip dataframe.cvs 
+files.download('result.zip ')
