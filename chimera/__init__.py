c89454d7b8ba2414c9db7bcbbe57629cf05779ea
Your Name <you@example.com>
2026-03-12 15:21:50 +0100

    missing commits

--- chimera/__init__.py
+++ chimera/__init__.py
@@ -174,7 +174,7 @@ class Plugin(pwem.Plugin):
             ("""cd .. && 
                 python %s %s""" % (getchimera_script, version),
                 "../ChimeraX-%s.flatpak" % version),
-            (f"""cd .. && pwd > /tmp/kk &&\
+            (f"""cd .. &&\
                  sudo flatpak install -y  ChimeraX-{version}.flatpak &&\
                  mkdir -p chimerax-{version}/bin &&\
                  echo 'flatpak run edu.ucsf.rbvi.ChimeraX $*'> chimerax-{version}/bin/ChimeraX""", extractionDir)]
